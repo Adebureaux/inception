1dbc05f02d4a742106d7778fa8d56a0860905eea
adeburea <adebureaux@gmail.com>
2022-08-14 18:27:01 +0200

    save

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 service mysql start
-
-# echo "CREATE DATABASE wordpress;" | mysql -u root
-# echo "CREATE USER 'admin' identified by 'admin';" | mysql -u root
-# echo "GRANT USAGE ON wordpress.* TO 'admin'@'localhost' identified by 'admin';" | mysql -u root
-# echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'localhost' identified by 'admin';" | mysql -u root
-# echo "FLUSH PRIVILEGES;" | mysql -u root
+echo "CREATE DATABASE wordpress;" | mysql -u root
+echo "CREATE USER 'admin' identified by 'admin';" | mysql -u root
+echo "GRANT USAGE ON wordpress.* TO 'admin'@'%' identified by 'admin';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'%' identified by 'admin';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
